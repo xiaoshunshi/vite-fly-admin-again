9eb956b68798c39e34582092414cfd125883de6d
xiaoshunshi <song159769@163.com>
2022-09-22 17:15:07 +0800

    feat: 添加element-plus

--- vite.config.ts
+++ vite.config.ts
@@ -15,5 +15,24 @@ export default defineConfig( {
     alias: {
       '@': resolve( 'src' )
     }
+  },
+  server: {
+    // 服务器主机名
+    // host: '',
+    // 端口号
+    port: 3088,
+    // 设为 true 时若端口已被占用则会直接退出，而不是尝试下一个可用端口
+    strictPort: false,
+    // 服务器启动时自动在浏览器中打开应用程序,当此值为字符串时，会被用作 URL 的路径名
+    open: true,
+    // 自定义代理规则
+    proxy: {
+      // 选项写法
+      '/api': {
+        target: 'https://www.fastmock.site/mock/935974cbe8285109b2162d84b5d52c3c/api',
+        changeOrigin: true,
+        rewrite: path => path.replace( /^\/api/, '' )
+      }
+    }
   }
 } )
