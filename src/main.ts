b4fd38892bb75a74452b76519d33c572bcdbc778
xiaoshunshi <song159769@163.com>
2022-09-30 09:04:33 +0800

    feat: 注册elementicon

--- src/main.ts
+++ src/main.ts
@@ -4,10 +4,22 @@ import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 import App from './App.vue'
 import pinia from './store'
+// 注册icon组件
+import * as ElIconsModules from '@element-plus/icons-vue'
 
 const app = createApp(App)
 
 app.use(ElementPlus)
+// 全局注册element-plus icon图标组件
+Object.keys(ElIconsModules).forEach((key) => {
+  // 循环遍历组件名称
+  if (key !== 'Menu') {
+    // 如果不是图标组件不是Menu，就跳过，否则加上ICon的后缀
+    app.component(key, ElIconsModules[key])
+  } else {
+    app.component(key + 'Icon', ElIconsModules[key])
+  }
+})
 app.use(pinia)
 app.use(router)
 app.mount('#app')
