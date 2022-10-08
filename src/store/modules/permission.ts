94c84bf17df7b21b2022a197999cb73056dd2973
xiaoshunshi <song159769@163.com>
2022-10-08 19:19:20 +0800

    feat: 路径匹配组件成功

--- src/store/modules/permission.ts
+++ src/store/modules/permission.ts
@@ -1,12 +1,17 @@
+import Layout from '@/layout/index.vue'
 import { permissionType } from './../types'
 import { defineStore } from 'pinia'
 import { getUserPermission } from '@/api/loign'
 import { constantRoutes } from '@/router'
 import { RouteRecordRaw } from 'vue-router'
 
+// 匹配所有的vue文件
+const modules = import.meta.glob('./../../views/**/*.vue')
+console.log(modules)
+
 export const usePermissionStore = defineStore({
   id: 'permissionState',
-  state: ():permissionType => ({
+  state: (): permissionType => ({
     // 路由
     routes: [],
     // 动态路由
@@ -24,19 +29,44 @@ export const usePermissionStore = defineStore({
     generateRoutes() {
       return new Promise(async resolve => {
         const result = await getUserPermission()
-        const accessedRoutes:Array<RouteRecordRaw> = this.changeComponent(result.data)
+        const accessedRoutes: Array<RouteRecordRaw> = this.changeComponent(result.data)
         this.routes = constantRoutes.concat(accessedRoutes as Array<RouteRecordRaw>)
         this.addRoutes = accessedRoutes
         resolve(accessedRoutes)
       })
     },
     // 把请求回来的conponent路径转换为组件
-    changeComponent(data:any) {
-      const result = data.map((item:any) => {
-        // item.component = import(item.component)
-        return item
+    changeComponent(data: any) {
+      data.forEach((item: any) => {
+        if (item.component) {
+          if (item.component === 'Layout') {
+            item.component = Layout
+          } else {
+            item.component = this.loadView(item)
+          }
+        }
+        if (item.children) {
+          this.changeComponent(item.children)
+        }
       })
-      return result
+      // const result = data.map((item:any) => {
+      //   // item.component = import(item.component)
+      //   return item
+      // })
+      // return result
+      return data
+    },
+    // 提取views里面的文件，找到对应的文件懒加载函数并执行
+    loadView(view: any) {
+      let res
+      for (const path in modules) {
+        const dir = '/' + path.split('views/')[1]
+        if (dir === view.component) {
+          res = () => modules[path]()
+        }
+      }
+      return res
     }
+
   }
 })
